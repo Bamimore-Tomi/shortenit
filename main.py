d36e449a3e2004298c3c4786feb0b5a193fb0ebb
Bamimore-Tomi <preciousgiftexpression@gmail.com>
2020-11-25 00:41:46 +0100

    Genesis

---
+++ main.py
@@ -0,0 +1,42 @@
+
+
+import os
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import pymongo
+from dotenv import load_dotenv
+from pydantic import BaseModel, HttpUrl
+import random
+
+load_dotenv()
+
+DB_URL = os.getenv('DB_URL')
+DB_NAME = os.getenv('DB_NAME')
+
+client = pymongo.MongoClient(DB_URL)
+db = client[DB_NAME]
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return "Welcome to rolo. Go to /docs to see API documentation."
+
+@app.get('/api/', response_model=HttpUrl)
+def shorten(url : HttpUrl):
+    id_ = ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ') for i in range(4))
+    if db.urls.find_one({'url':url})!=None:
+        id_ = db.urls.find_one({'url':url})['id']
+        url_ = 'https://maphor.herokuapp.com/p'+id_
+        return url_
+    else:
+        db.urls.insert_one({'id':id_,'url':url})
+        url_ = 'https://maphor.herokuapp.com/p/'+id_
+        return url_
+
+@app.get('/p/{id_}')
+def route(id_:str):
+    url = db.urls.find_one({'id':id_})['url']
+    return RedirectResponse(url,status_code=302)
+        
